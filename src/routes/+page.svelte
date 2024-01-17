e6d5ed6aea5acf50645fa4bea33b3f72f3fd5474
linvegas <saulo.costam@gmail.com>
2024-01-17 17:55:55 -0300

    list of boards on home page based on mock json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,44 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
 <h1>Welcome to Svelchan</h1>
-<h3>List of boards:</h3>
-<ul>
-  <li>/g</li>
-  <li>/v</li>
-  <li>/a</li>
-  <li>/b</li>
-</ul>
+<main class="container">
+  <h3>List of boards:</h3>
+  <ul>
+    {#each data.boards as board}
+      <li>
+        <span class="red">/{board.board}</span> -
+        <strong>{board.title}</strong>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+  main {
+    padding-block: 2rem;
+  }
+  h3 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 2rem;
+  }
+  li {
+    background: rebeccapurple;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+  }
+  .red {
+    color: lightgreen;
+  }
+</style>
