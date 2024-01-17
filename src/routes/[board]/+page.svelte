6f9c0b4ed4f750c982319b1fd91a820105c901a4
linvegas <saulo.costam@gmail.com>
2024-01-17 17:57:11 -0300

    prototype styles

--- src/routes/[board]/+page.svelte
+++ src/routes/[board]/+page.svelte
@@ -5,19 +5,45 @@
 
 <h1>Catalog Page</h1>
 
-<ul>
-  {#each data.threads as thread}
-    <li>
-      <h4>{thread.sub}</h4>
-      <p>{@html thread.com}</p>
-    </li>
-  {/each}
-</ul>
+<main class="container">
+  <ul>
+    {#each data.threads as thread}
+      <li>
+        <img
+          alt="Thumbnail"
+          src={`https://placehold.co/${thread.tn_w}x${thread.tn_h}`}
+          width={thread.tn_w}
+          height={thread.tn_h}
+          loading="lazy"
+        />
+        <h4>{thread.sub ? thread.sub : ""}</h4>
+        <p>{@html thread.com}</p>
+      </li>
+    {/each}
+  </ul>
+</main>
 
 <style>
+  main {
+    padding-block: 2rem;
+  }
+  h1 {
+    text-align: center;
+  }
+  ul {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fit, minmax(min(100%, 250px), 1fr));
+  }
   li {
     background: rebeccapurple;
-    padding: 2rem;
-    margin-bottom: 1rem;
+    padding: 1rem;
+    border-radius: 0.5rem;
+  }
+  p {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 4;
+    overflow: hidden;
   }
 </style>
--- src/routes/[board]/[thread]/+page.svelte
+++ src/routes/[board]/[thread]/+page.svelte
@@ -5,20 +5,29 @@
 
 <h1>Thread Page</h1>
 
-<ul>
-  {#each data.thread as thread}
-    <li>
-      <h4>{thread.sub ? thread.sub : ""}</h4>
-      <h4>{thread.name}</h4>
-      <p>{@html thread.com}</p>
-    </li>
-  {/each}
-</ul>
+<main class="container">
+  <ul>
+    {#each data.thread as thread}
+      <li>
+        <h4>{thread.sub ? thread.sub : ""}</h4>
+        <h4>{thread.name}</h4>
+        <p>{@html thread.com}</p>
+      </li>
+    {/each}
+  </ul>
+</main>
 
 <style>
+  h1 {
+    text-align: center;
+  }
+  main {
+    padding-block: 2rem;
+  }
   li {
     background: rebeccapurple;
-    padding: 2rem;
+    padding: 1rem;
+    border-radius: 0.5rem;
     margin-bottom: 1rem;
   }
 </style>
